1cfe5299adab93225b434d4f86e90b614a2762ae
cecilestaller <c.staller96@gmail.com>
2023-11-16 15:42:44 +0100

    picsum fetch project

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+console.log("%c picsum api level1_1", "color:tomato");
+
+fetch('https://picsum.photos/v2/list')
+    .then(response => {
+        console.log(response);
+        if (response.ok === false){
+            throw new Error("Sorry, da ist etwas schief gelaufen")
+        }
+        return response.json();
+    })
+    .then(imgArr => {
+        console.log(imgArr);
+        const sectionFetchGallery = document.getElementById('fetch-gallery');
+        // console.log(sectionFetchGallery);
+        imgArr.forEach((singleObj) => {
+            console.log(singleObj);
+            sectionFetchGallery.innerHTML += `
+            <figure>
+                <img src="${singleObj.download_url}" alt="picture from ${singleObj.author}">
+                <figcaption>${singleObj.author}</figcaption>
+                <a href="${singleObj.url}" target="_blank">See more</a>
+            </figure>`;
+        })
+    })
+    .catch(error => document.write(error));
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Project Picsum Fetch
+
+- This project was created as part of a coding bootcamp.
+- the learning objective of this project: learning how to use fetch and APIs
+- For this project, we should use Picsum's API to create a gallery via JavaScript.
+- Clicking on the button opens a new window
+- I have built error handling into my fetch
+
+## Demo
+
+## Screenshots
+
+![pap flowchart](./assets/img/screenshot_picsumFetch.png)
+
+## Tech Stack
+
+- HTML5
+- SCSS
+- JavaScript
+- RESTFUL API
+
+## Link to used API
+
+https://picsum.photos/v2/list
+
+## Author
+
+- [@cecilestaller](https://github.com/cecilestaller)
